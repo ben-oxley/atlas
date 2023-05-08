5d3a07731ad9a81c7db97173e4088369eca06ba4
Ben Oxley <ben.oxley@gmail.com>
2023-05-08 23:11:14 +0100

    Updated pipeline for higher resolution processing

--- atlas/image-collection-pipeline.py
+++ atlas/image-collection-pipeline.py
@@ -3,6 +3,7 @@
 from multiprocessing import freeze_support
 import numpy as np
 import xarray as xr
+import rioxarray
 
 import rasterio.features
 import stackstac
@@ -61,8 +62,7 @@ def process():
         stackstac.stack(
             items,
             assets=["B04", "B03", "B02"],  # red, green, blue
-            chunksize=4096,
-            resolution=100,
+            chunksize=4096
         )
         .where(lambda x: x > 0, other=np.nan)  # sentinel-2 uses 0 as nodata
         .assign_coords(band=lambda x: x.common_name.rename("band"))  # use common names
@@ -77,9 +77,12 @@ def process():
     fig, ax = plt.subplots(figsize=(8, 8))
 
     ax.set_axis_off()
+    image.transpose('band', 'y', 'x').rio.to_raster("image.tif")
     image.plot.imshow(ax=ax);
     monthly = data.groupby("time.month").median().compute()
     images = [ms.true_color(*x) for x in monthly]
+    for i in range(len(images)):
+        images[i].transpose('band', 'y', 'x').rio.to_raster(f"image-{i}.tif")
     images = xr.concat(images, dim="time")
 
     g = images.plot.imshow(x="x", y="y", rgb="band", col="time", col_wrap=3, figsize=(6, 8))
