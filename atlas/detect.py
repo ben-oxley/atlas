536a48118358c2f69a804a39abdd9f770161e9a8
Ben Oxley <ben.oxley@gmail.com>
2025-01-26 22:38:04 +0000

    Added metric aggregation routines

--- atlas/detect.py
+++ atlas/detect.py
@@ -1,10 +1,11 @@
-from typing import Dict
+import datetime
+from typing import Dict, Set
 from PIL import Image
 from ultralytics import YOLO
 from pathlib import Path
 
 from atlas.db import AtlasDBFacade
-from atlas.models.tile import Tile, TileMetricTimeline
+from atlas.models.tile import Tile, TileMetricDatapoint, TileMetricTimeline
 
 
 '''
@@ -47,13 +48,46 @@ def get_change_over_time(tile_x:int,tile_y:int,tile_z:int,metric:str):
     return dbcontext.get_metric(tile_x,tile_y,tile_z,metric)
 
 
-def get_distinct_times(tiles:Dict[Tile,TileMetricTimeline]):
-    times = set([
+def get_distinct_times(tiles:Dict[Tile,TileMetricTimeline])->Set[datetime]:
+    times:Set[datetime] = set([
         datapoint.time
         for tile in tiles.values()
-        for datapoints in tile.values
-        for datapoint in datapoints
+        for datapoint in tile.values
     ])
+    return times
+
+
+def find_matches_for_times(times:Set[datetime],tiles:Dict[Tile,TileMetricTimeline]):
+    matched_tiles = {}
+    for tile,tile_timeline in tiles.items():
+        values_iterator = 0
+        current_value = 0
+        timeline = TileMetricTimeline()
+        timeline.values = []
+        for time in times:
+            #There are two possibilities: 
+            # 1) there is a matching entry between time and the values for the time. 
+            # 2)There is not and the last match should be used, or 0 if no match
+            if time == tile_timeline.values[values_iterator].time:
+                #Then at this time, there is no data available for this tile, so 0
+                current_value = tile_timeline.values[values_iterator]
+                timeline.values.append(TileMetricDatapoint(time,current_value))
+                values_iterator += 1
+            else:
+                #then there is a tile for a point before the current time or equal to
+                timeline.values.append(TileMetricDatapoint(time,current_value))
+        matched_tiles[tile] = timeline
+    return matched_tiles
+
+def aggregate_results(times:Set[datetime],matched_tiles:dict[Tile,TileMetricTimeline]):
+    timeline = TileMetricTimeline()
+    for i in range(0,len(times)):
+        result = sum([v.values[i].count for v in matched_tiles.values()])
+        timeline.values.append(TileMetricDatapoint(times[i],result))
+
+
+
+
 
 
     
